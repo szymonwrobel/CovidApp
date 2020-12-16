d49882d2b3d3c4acc583f9c92d313d02a6308acf
Szymon Wróbel <szym.wrobel@gmail.com>
2020-12-16 01:34:45 +0100

    added changing text's color, properly for the mood of the according day. Changed moodText.text

--- app/src/main/java/com/example/covidapp/ui/history/HistoryListAdapter.kt
+++ app/src/main/java/com/example/covidapp/ui/history/HistoryListAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.covidapp.ui.history
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.example.covidapp.R
 import com.example.covidapp.model.HealthRecord
 import kotlinx.coroutines.GlobalScope
@@ -16,12 +18,11 @@ import kotlinx.coroutines.launch
 import java.util.*
 
 class HistoryListAdapter(
-    private val context: Context?,
+    private val context: Context,
     private val healthRecordList: ArrayList<HealthRecord>
 ) : BaseAdapter() {
 
     private val inflater: LayoutInflater = LayoutInflater.from(this.context)
-//        context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getCount(): Int {
         return healthRecordList.size
@@ -35,6 +36,7 @@ class HistoryListAdapter(
         return position.toLong()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         val rowView = inflater.inflate(R.layout.history_record_layout, parent, false)
@@ -45,21 +47,38 @@ class HistoryListAdapter(
         val riskText = rowView.findViewById<TextView>(R.id.riskText)
 
         val healthRecord = getItem(position) as HealthRecord
+        var imageResource = 0
 
-        val imageResource = when (healthRecord.overallMood) {
-            HealthRecord.MoodLevel.TERRIBLE -> R.drawable.ic_big_thumb_down_red_24dp
-            HealthRecord.MoodLevel.BAD -> R.drawable.ic_thumb_down_red_24dp
-            HealthRecord.MoodLevel.OK -> R.drawable.ic_thumbs_up_down_black_24dp
-            HealthRecord.MoodLevel.GOOD -> R.drawable.ic_thumb_up_green_24dp
-            HealthRecord.MoodLevel.EXCELLENT -> R.drawable.ic_big_thumb_up_green_24dp
+        when (healthRecord.overallMood) {
+            HealthRecord.MoodLevel.TERRIBLE -> {
+                imageResource = R.drawable.ic_big_thumb_down_red_24dp
+                moodText.setTextColor(context.getColor(R.color.my_kinda_red_variant))
+            }
+            HealthRecord.MoodLevel.BAD -> {
+                imageResource = R.drawable.ic_thumb_down_red_24dp
+                moodText.setTextColor(context.getColor(R.color.my_kinda_red_variant))
+            }
+            HealthRecord.MoodLevel.OK -> {
+                imageResource = R.drawable.ic_thumbs_up_down_black_24dp
+                moodText.setTextColor(Color.BLACK)
+            }
+            HealthRecord.MoodLevel.GOOD -> {
+                imageResource = R.drawable.ic_thumb_up_green_24dp
+                moodText.setTextColor(context.getColor(R.color.my_green_or_mint))
+            }
+            HealthRecord.MoodLevel.EXCELLENT -> {
+                imageResource = R.drawable.ic_big_thumb_up_green_24dp
+                moodText.setTextColor(context.getColor(R.color.my_green_or_mint))
+            }
         }
 
-        moodText.text = healthRecord.overallMood.name.toLowerCase(Locale.ROOT)
+        moodText.text = context.getString(
+            R.string.feelingInfo,
+            healthRecord.overallMood.name.toLowerCase(Locale.ROOT)
+        )
         dateText.text = healthRecord.getDateString()
         riskText.text = healthRecord.getRiskShortenedInfo()
-        GlobalScope.launch {
-            image.setImageResource(imageResource)
-        }
+        image.setImageResource(imageResource)
 
         return rowView
     }

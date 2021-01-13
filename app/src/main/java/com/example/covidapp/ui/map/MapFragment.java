e58c82dc7f516cc02c9106e3897e115e916b404c
Xarria <224340@edu.p.lodz.pl>
2021-01-13 23:03:06 +0100

    Add some icons for map, add getting current location to map

--- app/src/main/java/com/example/covidapp/ui/map/MapFragment.java
+++ app/src/main/java/com/example/covidapp/ui/map/MapFragment.java
@@ -2,50 +2,33 @@ package com.example.covidapp.ui.map;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
-import android.media.audiofx.BassBoost;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
-import android.os.Looper;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.example.covidapp.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-import java.util.Objects;
-
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements LocationListener{
 
     private SupportMapFragment supportMapFragment;
-    private FusedLocationProviderClient client;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,13 +36,10 @@ public class MapFragment extends Fragment {
 
 
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
         supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
-
-        client = LocationServices.getFusedLocationProviderClient(requireContext());
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
         getCurrentLocation();
-
 
         return view;
     }
@@ -69,9 +49,9 @@ public class MapFragment extends Fragment {
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions((Activity) requireContext(),
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},101);
         }
-        Task<Location> task = client.getLastLocation();
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
@@ -81,6 +61,7 @@ public class MapFragment extends Fragment {
                         public void onMapReady(GoogleMap googleMap) {
                             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                             MarkerOptions options = new MarkerOptions().position(latLng)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.me1))
                                     .title("You're here!");
                             googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                             googleMap.addMarker(options);
@@ -93,10 +74,14 @@ public class MapFragment extends Fragment {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 44){
+        if (requestCode == 101){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 getCurrentLocation();
             }
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
     }
 }

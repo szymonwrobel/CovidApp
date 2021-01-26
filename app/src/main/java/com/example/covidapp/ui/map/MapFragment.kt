96155139ce985c104a5fe34dba356ff3597b3a19
Szymon Wróbel <szym.wrobel@gmail.com>
2021-01-26 06:36:47 +0100

    tries to do sth with that localization

--- app/src/main/java/com/example/covidapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/covidapp/ui/map/MapFragment.kt
@@ -1,43 +1,101 @@
 package com.example.covidapp.ui.map
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import com.example.covidapp.R
 import com.example.covidapp.dao.ListOfMarkers
-import com.example.covidapp.model.SingleMarker
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+
+class MapFragment : Fragment(), OnMapReadyCallback, LocationListener {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var mapFragment: SupportMapFragment? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
-        val mapFragment = childFragmentManager
+        mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment!!.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
+        getCurrentLocation()
         return view
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                (requireContext() as Activity),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                101
+            )
+        }
+        val task: Task<Location> = fusedLocationClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                mapFragment?.getMapAsync { googleMap ->
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    val options = MarkerOptions().position(latLng)
+                        .title("You're here!")
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                    googleMap.addMarker(options)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 101) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            }
+        }
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
         val listOfMarkers = ListOfMarkers()
         for (marker in listOfMarkers.markers) {
-            googleMap.addMarker(
+            googleMap?.addMarker(
                 MarkerOptions().position(LatLng(marker.latitude, marker.longitude))
                     .title(marker.name)
             )
         }
     }
-
 }
 
 
